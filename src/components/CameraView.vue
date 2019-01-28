e373f64d57cf0db2cb2aec10da678c4439c07009
bdurb <brian@rpmgreenville.com>
2019-01-28 14:48:30 -0800

    added temeplate and css properties for cameraview

--- src/components/CameraView.vue
+++ src/components/CameraView.vue
@@ -1,5 +1,7 @@
 <template>
-    <div></div>
+    <div class="camera-modal">
+      <video ref="video" class="camera-stream"/>
+    </div>
 </template>
 
 <script>
@@ -8,4 +10,17 @@
 </script>
 
 <style scoped>
+  .camera-modal {
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        position: absolute;
+        background-color: white;
+        z-index: 10;
+    }
+    .camera-stream {
+        width: 100%;
+        max-height: 100%;
+    }
 </style>
