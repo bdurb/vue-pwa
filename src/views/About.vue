40ebfeb35ef577c3361bd79e9cc7ecf9b4ad0a2a
bdurb <brian@rpmgreenville.com>
2019-01-28 17:18:04 -0800

    added to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About This App</h1>
+    <h4>Currently, Its a PWA that can access your camera.</h4>
   </div>
 </template>
